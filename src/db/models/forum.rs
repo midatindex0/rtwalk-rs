4a670847a5877a94858f18fd32de3687c26d72af
lonely-code-cube <lonely-code-cube@github.com>
2023-06-14 19:36:00 +0530

    added owner field in forum

--- src/db/models/forum.rs
+++ src/db/models/forum.rs
@@ -1,19 +1,33 @@
-use async_graphql::SimpleObject;
+use async_graphql::{ComplexObject, Context, SimpleObject};
 use chrono::NaiveDateTime;
 use diesel::prelude::*;
 
 use crate::db::models::user::User;
-use crate::schema::forums;
+use crate::db::pool::PostgresPool;
+use crate::schema::{forums, users};
 
 #[derive(Queryable, Selectable, Insertable, Debug, Associations, SimpleObject)]
 #[diesel(belongs_to(User, foreign_key=owner_id))]
 #[diesel(table_name=forums)]
+#[graphql(complex)]
 pub struct Forum {
     pub id: i32,
     pub name: String,
     pub description: Option<String>,
     pub created_at: NaiveDateTime,
     pub owner_id: i32,
+}
+
+#[ComplexObject]
+impl Forum {
+    async fn owner<'ctx>(&self, ctx: &Context<'ctx>) -> async_graphql::Result<User> {
+        let pool = ctx.data::<PostgresPool>().unwrap();
+        let mut conn = pool.get()?;
+        let owner = users::table
+            .filter(users::dsl::id.eq(self.owner_id))
+            .get_result::<User>(&mut conn)?;
+        Ok(owner)
+    }
 }
 
 /// Represents a new forum that will be inserted into the db

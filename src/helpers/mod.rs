1c582d9e9b73c01834d800cb8ef989b03197f11e
lonely-code-cube <lonely-code-cube@github.com>
2023-07-13 10:11:49 +0530

    static allowed chars hashset

--- src/helpers/mod.rs
+++ src/helpers/mod.rs
@@ -1,5 +1,7 @@
 use std::collections::HashSet;
 
+use once_cell::sync::Lazy;
+
 use crate::constants::RESERVED_USERNAMES;
 
 #[macro_export]
@@ -18,15 +20,23 @@ pub fn check_reserved_username(username: &str) -> bool {
     RESERVED_USERNAMES.contains(&username)
 }
 
-pub fn check_valid_uservane(username: &str) -> bool {
-    let allowed_chars = HashSet::from([
+static ALLOWED_USERNAME_CHARS: Lazy<HashSet<char>> = Lazy::new(|| {
+    HashSet::from([
         'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
         's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
         '_',
-    ]);
+    ])
+});
+
+pub fn check_valid_uservane(username: &str) -> bool {
     let username: HashSet<char> = username.chars().collect();
 
-    if allowed_chars.union(&username).collect::<Vec<_>>().len() > allowed_chars.len() {
+    if ALLOWED_USERNAME_CHARS
+        .union(&username)
+        .collect::<Vec<_>>()
+        .len()
+        != ALLOWED_USERNAME_CHARS.len()
+    {
         return false;
     }
     true
